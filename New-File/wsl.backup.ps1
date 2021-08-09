ab132ca9c2725c4dfb023ea80e88e6fd3e8f1d5e
John F Vanden Bosch Jr <75092070+j1fvandenbosch@users.noreply.github.com>
2021-08-08 20:15:36 -0600

    added proxmox info

--- New-File/wsl.backup.ps1
+++
@@ -1,4 +0,0 @@
-wsl.exe --export Ubuntu-20.04 C:\Users\jfvandenbosch\Downloads\ubuntu-backup.tar.gz
-
-wsl.exe --export kali-linux C:\Users\jfvandenbosch\Downloads\kali-backup.tar.gz
-
---
+++ Windows/wsl.backup.ps1
@@ -0,0 +1,4 @@
+wsl.exe --export Ubuntu-20.04 C:\Users\jfvandenbosch\Downloads\ubuntu-backup.tar.gz
+
+wsl.exe --export kali-linux C:\Users\jfvandenbosch\Downloads\kali-backup.tar.gz
+
